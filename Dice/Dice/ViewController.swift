2aad0829bd46831d95e04a9bcaf1ed2a76148426
Vladyslav <savonikvladyslav@gmail.com>
2021-04-18 04:17:38 +0300

    Dice game

---
+++ Dice/Dice/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by MACsimus on 18.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstDice: UIImageView!
+    @IBOutlet weak var secondDice: UIImageView!
+    
+    var firstDiceNumber = 1
+    var secondDiceNumber = 5
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func rollAction(_ sender: Any) {
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        firstDice.image = diceArray.randomElement()
+        secondDice.image = diceArray.randomElement()
+    }
+    
+
+
+}
+
